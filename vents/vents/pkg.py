e8c879cb147a71cadd43cedd955b97df61d70448
mouradmourafiq <mouradmourafiq@gmail.com>
2023-05-05 10:50:07 +0200

    Bump version to v0.1.0

--- vents/vents/pkg.py
+++ vents/vents/pkg.py
@@ -1,5 +1,5 @@
 NAME = "vents"
-VERSION = "0.0.7"
+VERSION = "0.1.0"
 DESC = "Open source integrations, alerting, and notification library."
 URL = "https://github.com/mmourafiq/vents"
 AUTHOR = "Mourad Mourafiq"
